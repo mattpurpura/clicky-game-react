21da3e859cbdb9f5ce1d23ae096e83fe7fb3665d
mattpurpura <mpurpura212@gmail.com>
2018-10-23 15:04:33 -0700

    Updates

---
+++ precache-manifest.7fe61b6f01948290f8539201ee76c951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ffa1f4187ecf54ec1d4",
+    "url": "/clicky-game-react/static/js/runtime~main.1ffa1f41.js"
+  },
+  {
+    "revision": "73f81035caa3135497fe",
+    "url": "/clicky-game-react/static/js/main.73f81035.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clicky-game-react/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "73f81035caa3135497fe",
+    "url": "/clicky-game-react/static/css/main.5586c4ce.chunk.css"
+  },
+  {
+    "revision": "d8713c42f8db288b2b5d40098022781c",
+    "url": "/clicky-game-react/index.html"
+  }
+];
